db05be16e718a65a1b1e345d8ffd4a7a2a3249e9
James Yu <james@Jamess-MBP.attlocal.net>
2022-11-25 03:21:17 -0800

    set up server with flask-mail code. URL is https://g3vyfd.deta.dev/

---
+++ backend/my_micro/main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'cs166team1@gmail.com'
+app.config['MAIL_PASSWORD'] = 'oiqypvbuoabcrbpy'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+@app.route('/home', methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
+def home():
+    if request.method == 'POST':
+        msg = Message(
+            "Hey", 
+            sender='noreply@demo.com', 
+            recipients=['james.yu@sjsu.edu']
+            #recipients=
+            #[
+            #    'shohin.abdulkhamidov@sjsu.edu', 'tejas.aditya@sjsu.edu', 'zaria.baker@sjsu.edu', 'jonathan.borda@sjsu.edu', 'hohin.chan@sjsu.edu', 
+            #    'tommy.dao@sjsu.edu', 'anh.dinh@sjsu.edu', 'eivind.eckhoff@sjsu.edu', 'ask.ekrareistad@sjsu.edu', 'harrison.yu@sjsu.edu',
+            #    'vidhyut.gopinath@sjsu.edu', 'sriram.govindan@sjsu.edu', 'samanthaelaine.guanzon@sjsu.edu', 'yunseo.han@sjsu.edu',
+            #    'ryan.hedgecock@sjsu.edu', 'elizabeth.huelfenhaus@sjsu.edu', 'sahana.ilenchezhian@sjsu.edu', 'muhammad.a.jeelani@sjsu.edu',
+            #    'abhayjot.johal@sjsu.edu', 'jason.khy@sjsu.edu', 'isaac.kim@sjsu.edu', 'ryan.kwong@sjsu.edu', 'victor.la@sjsu.edu',
+            #    'joshua.lawson@sjsu.edu', 'thuynhatphuong.le@sjsu.edu', 'vivian.letran@sjsu.edu', 'tristan.lorenzo@sjsu.edu', 'dylan.zeglinski@sjsu.edu',
+            #    'alan.luu@sjsu.edu', 'ali.ma@sjsu.edu', 'lexin.ma@sjsu.edu', 'volodymyr.makarenko@sjsu.edu', 'farah.masood@sjsu.edu',
+            #    'siri.mudumbi@sjsu.edu', 'arun.murugan@sjsu.edu', 'anjana.nambiar@sjsu.edu', 'huy.ong@sjsu.edu', 'cody.ourique@sjsu.edu',
+            #    'danh.pham@sjsu.edu', 'doan.pham@sjsu.edu', 'eric.p.pham@sjsu.edu', 'truc.t.phan@sjsu.edu', 'chiranjeev.puri@sjsu.edu',
+            #    'praggathi.rajarao@sjsu.edu', 'austin.rivard@sjsu.edu', 'vladislav.semenyutin@sjsu.edu', 'temuudei.shiilegdamba@sjsu.edu',
+            #    'harmandeepsingh@sjsu.edu', 'inderpreet.singh01@sjsu.edu', 'hyeonmin.song@sjsu.edu', 'jimin.song@sjsu.edu', 'steven.ta@sjsu.edu',
+            #    'lovepreet.uppal@sjsu.edu', 'saharsh.vedi@sjsu.edu', 'karanpartap.virk@sjsu.edu', 'angela.yang@sjsu.edu', 'atsuya.yano@sjsu.edu'
+            #    'justin.barragan@sjsu.edu', 'zayd.kudaimi@sjsu.edu', 'san.vu@sjsu.edu', 'james.yu@sjsu.edu'
+            #]
+        )
+        msg.subject = "Subject"
+        link = url_for('home', _external=True)
+        msg.body = '{}'.format(link)
+        mail.send(msg)
+        return "Sent email."
+    return render_template('index.html')
